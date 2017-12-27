c08d6356cf3059f614e772a5607f35a8b640e584
Filip Gralinski <filipg@amu.edu.pl>
2017-12-27 16:36:49 +0100

    add tests, WVector type synonym

--- src/Data/Word2Vec/Model.hs
+++ src/Data/Word2Vec/Model.hs
@@ -4,19 +4,26 @@
 module Data.Word2Vec.Model
     ( readWord2VecModel
     , getVector
+    , cosineSimilarity
+    , dotProduct
+    , WVector
     ) where
 
 import qualified Data.HashMap.Strict as DHS
 import Data.Text
 import Data.Text.Encoding
-import Data.Vector.Storable
+import Data.Vector.Storable as V
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Internal as BS
 import qualified Data.Attoparsec.ByteString.Lazy as AL
 import qualified Data.Attoparsec.ByteString.Char8 as AP
 import Data.Binary
 import Data.Binary.Get
 import Data.Binary.IEEE754
-data Word2VecModel = Word2VecModel Int Int !(DHS.HashMap Text (Vector Float))
+
+type WVector = Vector Float
+
+data Word2VecModel = Word2VecModel Int Int !(DHS.HashMap Text WVector)
                      deriving (Eq, Show)
 
 readWord2VecModel :: FilePath -> IO (Word2VecModel)
@@ -24,7 +31,7 @@ readWord2VecModel fileName = do
   contents <- BL.readFile fileName
   pure $ processWord2VecBinaryModel contents
 
-getVector :: Word2VecModel -> Text -> Maybe (Vector Float)
+getVector :: Word2VecModel -> Text -> Maybe WVector
 getVector (Word2VecModel _ _ h) w = DHS.lookup w h
 
 processWord2VecBinaryModel :: BL.ByteString -> Word2VecModel
@@ -45,11 +52,20 @@ parseWord2VecBinaryModel = do
 floatSize :: Int
 floatSize = 4
 
-parseWord2VecEntry :: Int -> AP.Parser (Text, Vector Float)
+parseWord2VecEntry :: Int -> AP.Parser (Text, WVector)
 parseWord2VecEntry nbOfDimensions = do
   word <- AP.takeWhile1 (not . AP.isSpace)
   " "
   floatVectorRaw <- AP.take (floatSize * nbOfDimensions)
-  return (decodeUtf8 word, buildFloatVector nbOfDimensions floatVectorRaw)
+  return (decodeUtf8 word, bytesToFloats floatVectorRaw)
 
-buildFloatVector nbOfDimensions !bs = runGet (replicateM nbOfDimensions getFloathost) $ BL.fromStrict bs
+bytesToFloats :: BS.ByteString -> WVector
+bytesToFloats = unsafeCast . aux . BS.toForeignPtr
+  where aux (fp,offset,len) = unsafeFromForeignPtr fp offset len
+
+cosineSimilarity :: WVector -> WVector -> Float
+cosineSimilarity veca vecb = (dotProduct veca vecb) / (sqrt ((norm veca) * (norm vecb)))
+  where norm v = V.sum $ V.map (**2.0) v
+
+dotProduct :: WVector -> WVector -> Float
+dotProduct veca vecb = V.sum $ V.zipWith (*) veca vecb
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,39 @@
+import Test.Hspec
+
+import Data.Word2Vec.Model
+
+import qualified Data.Vector.Storable as V
+
+import qualified Test.HUnit as HU
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "basic facilities" $ do
+    it "trivial dot product" $ do
+      dotProduct (V.fromList [1.0, 2.0]) (V.fromList [1.0, 2.0]) `shouldBeAlmost` 5.0
+    it "dot product" $ do
+      dotProduct (V.fromList [0.5, 0.0, -2.0]) (V.fromList [3.0, 5.3, 1.0]) `shouldBeAlmost` (-0.5)
+    it "cosine similarity (dissimilar)" $ do
+      cosineSimilarity (V.fromList [0.0, 4.3]) (V.fromList [2.7, 0.0]) `shouldBeAlmost` 0.0
+    it "cosine similarity (perfect similarity — trivial)" $ do
+      cosineSimilarity (V.fromList [1.0, 2.0]) (V.fromList [1.0, 2.0]) `shouldBeAlmost` 1.0
+    it "cosine similarity (perfect similarity)" $ do
+      cosineSimilarity (V.fromList [1.0, -3.7]) (V.fromList [2.0, -7.4]) `shouldBeAlmost` 1.0
+
+
+class AEq a where
+    (=~) :: a -> a -> Bool
+
+instance AEq Float where
+    x =~ y = abs ( x - y ) < (1.0e-4 :: Float)
+
+(@=~?) :: (Show a, AEq a) => a -> a -> HU.Assertion
+(@=~?) actual expected = expected =~ actual HU.@? assertionMsg
+    where
+      assertionMsg = "Expected : " ++ show expected ++
+                     "\nActual   : " ++ show actual
+
+shouldBeAlmost got expected = got @=~? expected
+
+shouldReturnAlmost :: (AEq a, Show a, Eq a) => IO a -> a -> Expectation
+shouldReturnAlmost action expected = action >>= (@=~? expected)
